2103efd741cedb1561d991b74b7303626be60570
D3muga <blakewai56@gmail.com>
2026-03-24 19:32:33 +0300

    CreateUsingEntityFrameWork and table Добавьте файлы проекта.

---
+++ SalonProject/FolderData/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonProject.FolderData
+{
+    public partial class SalonEntities: DbContext
+    {
+        private static SalonEntities context;
+
+        public static SalonEntities GetContex()
+        {
+            if (context == null)
+            {
+                context = new SalonEntities();
+            }
+            return context;
+        }
+    }
+}
---
+++ SalonProject/Pages/MainPages/MainPanel.xaml.cs
@@ -0,0 +1,98 @@
+﻿using SalonProject.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalonProject.Pages.MainPages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPanel.xaml
+    /// </summary>
+    public partial class MainPanel : Page
+    {
+        public static int info_input_bt;
+        public MainPanel()
+        {
+            InitializeComponent();
+            InformationTextPanel("Главная страница");
+        }
+        private void InformationTextPanel(string Text)
+        {
+            var mainWindows = Application.Current.Windows
+                              .OfType<MainWindow>()
+                              .FirstOrDefault();
+            if (mainWindows?.InformationPanel != null)
+            {
+                mainWindows.InformationPanel.Title = Text;
+            }
+        }
+        private void BtnManager_Click(object sender, RoutedEventArgs e)
+        {
+            info_input_bt = 1;
+            InformationTextPanel("Менеджеры");
+
+            FrameUser.Navigate(new Uri("Frame/PagesAdmin/Manager.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            info_input_bt = 2;
+            InformationTextPanel("Сотрудники");
+
+
+            FrameUser.Navigate(new Uri("Frame/PagesAllUser/Profile.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnService_Click(object sender, RoutedEventArgs e)
+        {
+            InformationTextPanel("Услуги");
+
+
+            FrameUser.Navigate(new Uri("Frame/PagesAllUser/Profile.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnInterior_Click(object sender, RoutedEventArgs e)
+        {
+            InformationTextPanel("Салон");
+
+
+            FrameUser.Navigate(new Uri("Frame/PagesAllUser/Profile.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnClient_Click(object sender, RoutedEventArgs e)
+        {
+            InformationTextPanel("Клиент");
+
+            FrameUser.Navigate(new Uri("Frame/PagesAllUser/Profile.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnProfile_Click(object sender, RoutedEventArgs e)
+        {
+            InformationTextPanel("Профиль");
+
+            FrameUser.Navigate(new Uri("Frame/PagesAllUser/Profile.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnExitAd_Click(object sender, RoutedEventArgs e)
+        {
+            InformationTextPanel("Данил Колбасенко!");
+            var resultExit = MessageBox.Show("Вы уверены что хотите выйти?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (resultExit == MessageBoxResult.Yes)
+            {
+                this.NavigationService.Navigate(new Uri("Frame/MainPages/Authorization.xaml", UriKind.RelativeOrAbsolute));
+                InformationTextPanel("Отмена выхода!");
+            }
+        }
+    }
+}
